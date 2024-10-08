e709338751815556ff043fd2ccdb9ba8da51080e
gowri <gowristhampy@gmail.com>
2024-10-08 10:01:34 +0530

    what

--- Python/Machinelearning/test.py
+++ Python/Machinelearning/test.py
@@ -3,4 +3,4 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
-df = pd.read_csv('2001-2017-financial-analysis-1.csv')
+df = pd.read_csv('/Users/gowrit/Desktop/path_to_file/FRUITS DATASET.csv')
