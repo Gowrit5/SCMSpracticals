e17661685affce12828bd534e03855f619b6e2ee
“gowri” <gowristhampy@gmail.com>
2024-11-08 18:44:22 +0530

    update

--- Python/Machinelearning/test.py
+++ Python/Machinelearning/test.py
@@ -3,4 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
-df = pd.read_csv('/Users/gowrit/Desktop/path_to_file/FRUITS DATASET.csv')
+df = pd.read_csv('Python/Machinelearning/Social_Network_Ads.csv')
+
